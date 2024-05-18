175fe88c90b8bca70a046358765e7e0d320fe118
Darren Sandhu <darrensandhu7@gmail.com>
2024-05-18 03:08:33 +0100

    Add serializers

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Education, Experience, Project
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'groups']
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = '__all__'
+
+class ExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experience
+        fields = '__all__'
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
