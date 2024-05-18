0eca16636c084d980a7928bb4c57233f818c5499
Darren Sandhu <darrensandhu7@gmail.com>
2024-05-18 03:06:34 +0100

    Add models and make migrations to database

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Education(models.Model):
+    degree = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    year = models.IntegerField()
+    description = models.TextField()
+    ordinal = models.IntegerField()
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    year = models.IntegerField()
+    description = models.TextField()
+    ordinal = models.IntegerField()
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='uploads/')
+    url = models.URLField()
+    ordinal = models.IntegerField()
