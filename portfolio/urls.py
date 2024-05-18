00ddd65337447633ac8fbf6311097eee286ec078
Darren Sandhu <darrensandhu7@gmail.com>
2024-05-18 03:13:27 +0100

    Add url routes and add views

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'education', views.EducationViewSet)
+router.register(r'experience', views.ExperienceViewSet)
+router.register(r'projects', views.ProjectViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
-from .serializers import UserSerializer
+from .serializers import UserSerializer, EducationSerializer, ExperienceSerializer, ProjectSerializer
+from .models import Education, Experience, Project
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -13,3 +14,26 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+class EducationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Education.objects.all().order_by('-date_joined')
+    serializer_class = EducationSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ExperienceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Experience.objects.all().order_by('-date_joined')
+    serializer_class = ExperienceSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Project.objects.all().order_by('-date_joined')
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
