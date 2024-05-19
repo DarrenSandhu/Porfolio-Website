29b98a42485dc65786e3dfea74db97f46ff2d369
Darren Sandhu <darrensandhu7@gmail.com>
2024-05-19 18:11:07 +0100

    Create HeroSection And Navbar

---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="w-1/2 mx-auto text-center text-2xl">
+        Home
+        </div>
+    );
+};
+
+export default Navbar;
