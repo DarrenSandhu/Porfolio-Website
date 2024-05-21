52b1c90cae4e1a0e375c9a1281570c3d1989d22e
Darren Sandhu <darrensandhu7@gmail.com>
2024-05-21 14:13:40 +0100

    Create pages styled components js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,28 +3,7 @@ import styled from 'styled-components';
 import HomeSection from '../components/HeroSection';
 import { Home } from '@mui/icons-material';
 import Skills from '../components/Skills';
-
-const Body = styled.div`
-  background-color: ${({theme}) => theme.bg};
-  width: 100%;
-  height: 100%;
-  overflow-x: hidden;
-`;
-
-const Wrapper = styled.div`
-  background: linear-gradient(
-    38.73deg,
-    rgba(135, 206, 235, 0.15) 0%,  // Light Sky Blue with slight transparency
-    rgba(135, 206, 250, 0) 50%     // Lighter Sky Blue, fading to transparent
-  ),
-  linear-gradient(
-    141.27deg,
-    rgba(240, 128, 128, 0) 50%,    // Light Coral, fading to transparent
-    rgba(240, 128, 128, 0.15) 100% // Light Coral with slight transparency
-  );
-  width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
-`;
+import { Body, Wrapper } from './PagesStyledComponents';
 
 
 
--- frontend/src/pages/ProjectsPage.js
+++ frontend/src/pages/ProjectsPage.js
@@ -2,23 +2,7 @@ import { useState, useEffect } from "react";
 import styled from 'styled-components';
 import Projects from '../components/Projects';
 import ProjectDetails from '../components/Projects/ProjectDetails';
-
-
-const Wrapper = styled.div`
-  background: linear-gradient(
-    38.73deg,
-    rgba(135, 206, 235, 0.15) 0%,  // Light Sky Blue with slight transparency
-    rgba(135, 206, 250, 0) 50%     // Lighter Sky Blue, fading to transparent
-  ),
-  linear-gradient(
-    141.27deg,
-    rgba(240, 128, 128, 0) 50%,    // Light Coral, fading to transparent
-    rgba(240, 128, 128, 0.15) 100% // Light Coral with slight transparency
-  );
-  width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
-`;
-
+import { Wrapper } from './PagesStyledComponents';
 
 
 const ProjectsPage = () => {
