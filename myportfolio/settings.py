c3088e0a74947d07a0e8650cf7bc46fe1de32c83
Darren Sandhu <darrensandhu7@gmail.com>
2024-05-21 15:59:07 +0100

    Create staticfiles directory

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
