b23db720551de673cf86a709b97a9661dd9b090e
Darren Sandhu <darrensandhu7@gmail.com>
2024-11-17 20:07:02 +0000

    Use api calls from seperate file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,78 +12,26 @@ import ErrorPage from './pages/ErrorPage';
 import Footer from './components/Footer';
 import { useState, useEffect } from 'react';
 import MotionParticleBackground from './components/3DBackground/MotionBackground';
+// import { userData } from './data/userData';
+
+import { useApi } from './data/apiUserData';
 
 
 function App() {
-  const [userData, setUserData] = useState({});
-  const [projectsData, setProjectData] = useState([]);
-  const [categoriesData, setCategoriesData] = useState([]);
-
-  useEffect(() => {
-    fetchUserData();
-    fetchProjectData();
-    fetchCategorytData();
-  }, []);
-
-  const fetchUserData = async () => {
-    try {
-      const response = await fetch('https://api.darrensandhu.uk/users/', {
-        headers: {
-          Authorization: 'Basic ' + btoa('darren:0800')
-        }
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch user data');
-      }
-      const userData = await response.json();
-      const user = userData[0];
-      setUserData(user);
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-    }
-  };
-
-  const fetchProjectData = async () => {
-    try {
-      const response = await fetch('https://api.darrensandhu.uk/projects/');
-      if (!response.ok) {
-        throw new Error('Failed to fetch project data');
-      }
-      const projectData = await response.json();
-      setProjectData(projectData);
-    } catch (error) {
-      console.error('Error fetching project data:', error);
-    }
-  };
-
-  const fetchCategorytData = async () => {
-    try {
-      const response = await fetch('https://api.darrensandhu.uk/categories/');
-      if (!response.ok) {
-        throw new Error('Failed to fetch category data');
-      }
-      const categoryData = await response.json();
-      const categoryNames = categoryData.map((category) => category.name);
-      const filteredCategoryNames = ['all', ...categoryNames];
-      setCategoriesData(filteredCategoryNames);
-    } catch (error) {
-      console.error('Error fetching category data:', error);
-    }
-  };
+  const {education, programming, user, skills, experience, projects, categories} = useApi();
 
   return (
     <ThemeProvider theme={lightTheme}>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage userData={userData} />} />
-          <Route path="/projects" element={<ProjectsPage projectsData={projectsData} user={userData} categories={categoriesData} />} />
-          <Route path="/contact" element={<ContactPage user={userData} />} />
-          <Route path="/resume" element={<ResumePage user={userData} />} />
+          <Route path="/" element={<HomePage userData={user} />} />
+          <Route path="/projects" element={<ProjectsPage projectsData={projects} user={user} categories={categories} />} />
+          <Route path="/contact" element={<ContactPage user={user} />} />
+          <Route path="/resume" element={<ResumePage user={user} />} />
           <Route path="*" element={<ErrorPage />}/>
         </Routes>
-        <Footer user={userData} />
+        <Footer user={user} />
       </Router>
     </ThemeProvider>
   );
--- frontend/src/data/apiUserData.js
+++ frontend/src/data/apiUserData.js
@@ -7,10 +7,31 @@ export const useApi = () => {
   const [skills, setSkills] = useState([]);
   const [experience, setExperience] = useState([]);
   const [projects, setProjects] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     fetchData();
+    fetchUserData();
   }, []);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch('https://api.darrensandhu.uk/users/', {
+        headers: {
+          Authorization: 'Basic ' + btoa('darren:0800')
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch user data');
+      }
+      const userData = await response.json();
+      const user = userData[0];
+      setUser(user);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
 
   const fetchData = async () => {
     try {
@@ -18,33 +39,36 @@ export const useApi = () => {
         Authorization: 'Basic ' + btoa('darren:0800')
       };
 
-      const educationResponse = await fetch('http://api.darrensandhu.uk/education/', { headers });
+      const educationResponse = await fetch('https://api.darrensandhu.uk/education/', { headers });
       const educationData = await educationResponse.json();
       setEducations(educationData);
 
-      const programmingResponse = await fetch('http://api.darrensandhu.uk/programming/', { headers });
+      const programmingResponse = await fetch('https://api.darrensandhu.uk/programming/', { headers });
       const programmingData = await programmingResponse.json();
       setProgramming(programmingData);
 
-      const userResponse = await fetch('http://api.darrensandhu.uk/users/', { headers });
-      const userData = await userResponse.json();
-      setUser(userData);
-
-      const skillsResponse = await fetch('http://api.darrensandhu.uk/skills/', { headers });
+      const skillsResponse = await fetch('https://api.darrensandhu.uk/skills/', { headers });
       const skillsData = await skillsResponse.json();
       setSkills(skillsData);
 
-      const experienceResponse = await fetch('http://api.darrensandhu.uk/experience/', { headers });
+      const experienceResponse = await fetch('https://api.darrensandhu.uk/experience/', { headers });
       const experienceData = await experienceResponse.json();
       setExperience(experienceData);
 
-      const projectsResponse = await fetch('http://api.darrensandhu.uk/projects/', { headers });
+      const projectsResponse = await fetch('https://api.darrensandhu.uk/projects/', { headers });
       const projectsData = await projectsResponse.json();
       setProjects(projectsData);
+
+      const response = await fetch('https://api.darrensandhu.uk/categories/');
+      const categoryData = await response.json();
+      const categoryNames = categoryData.map((category) => category.name);
+      const filteredCategoryNames = ['all', ...categoryNames];
+      setCategories(filteredCategoryNames);
+
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
-  return { educations, programming, user, skills, experience, projects };
+  return { educations, programming, user, skills, experience, projects, categories };
 };
